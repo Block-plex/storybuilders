8bbefcd1348b9ecd4d3a85d0bde9df78066c3ecb
Block-plex <blockplexsupp@outlook.com>
2026-07-22 14:01:39 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     next();
 });
+app.options("*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.sendStatus(200);
+});
 
 // Cloudflare R2 client
 const s3 = new S3Client({
